6688e9ba410968adaaca07ea4822e42b2d0afcad
ChevisOfficial <69173016+ChevisOfficial@users.noreply.github.com>
2023-09-04 22:54:42 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace MoarBytes
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SelectFile = new System.Windows.Forms.Button();
+            this.FileNameLabel = new System.Windows.Forms.Label();
+            this.FileSizeLabel = new System.Windows.Forms.Label();
+            this.SaveFile = new System.Windows.Forms.Button();
+            this.CountMb = new System.Windows.Forms.NumericUpDown();
+            this.AddBytesLabel = new System.Windows.Forms.Label();
+            this.OutputSizeLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CountMb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SelectFile
+            // 
+            this.SelectFile.BackColor = System.Drawing.Color.Transparent;
+            this.SelectFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SelectFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SelectFile.Location = new System.Drawing.Point(12, 55);
+            this.SelectFile.Name = "SelectFile";
+            this.SelectFile.Size = new System.Drawing.Size(168, 40);
+            this.SelectFile.TabIndex = 0;
+            this.SelectFile.TabStop = false;
+            this.SelectFile.Text = "Выбрать файл";
+            this.SelectFile.UseVisualStyleBackColor = false;
+            this.SelectFile.Click += new System.EventHandler(this.SelectFile_Click);
+            // 
+            // FileNameLabel
+            // 
+            this.FileNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FileNameLabel.ForeColor = System.Drawing.Color.Red;
+            this.FileNameLabel.Location = new System.Drawing.Point(12, 9);
+            this.FileNameLabel.Name = "FileNameLabel";
+            this.FileNameLabel.Size = new System.Drawing.Size(168, 43);
+            this.FileNameLabel.TabIndex = 1;
+            this.FileNameLabel.Text = "Файл: Не выбран";
+            this.FileNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FileSizeLabel
+            // 
+            this.FileSizeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FileSizeLabel.ForeColor = System.Drawing.Color.Blue;
+            this.FileSizeLabel.Location = new System.Drawing.Point(12, 98);
+            this.FileSizeLabel.Name = "FileSizeLabel";
+            this.FileSizeLabel.Size = new System.Drawing.Size(168, 43);
+            this.FileSizeLabel.TabIndex = 3;
+            this.FileSizeLabel.Text = "Размер файла: -";
+            this.FileSizeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SaveFile
+            // 
+            this.SaveFile.BackColor = System.Drawing.Color.Transparent;
+            this.SaveFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SaveFile.Location = new System.Drawing.Point(12, 187);
+            this.SaveFile.Name = "SaveFile";
+            this.SaveFile.Size = new System.Drawing.Size(168, 40);
+            this.SaveFile.TabIndex = 4;
+            this.SaveFile.TabStop = false;
+            this.SaveFile.Text = "Сохранить файл";
+            this.SaveFile.UseVisualStyleBackColor = false;
+            this.SaveFile.Click += new System.EventHandler(this.SaveFile_Click);
+            // 
+            // CountMb
+            // 
+            this.CountMb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.CountMb.Location = new System.Drawing.Point(102, 149);
+            this.CountMb.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.CountMb.Name = "CountMb";
+            this.CountMb.Size = new System.Drawing.Size(78, 26);
+            this.CountMb.TabIndex = 5;
+            this.CountMb.TabStop = false;
+            this.CountMb.ValueChanged += new System.EventHandler(this.CountMb_ValueChanged);
+            // 
+            // AddBytesLabel
+            // 
+            this.AddBytesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddBytesLabel.ForeColor = System.Drawing.Color.Blue;
+            this.AddBytesLabel.Location = new System.Drawing.Point(12, 141);
+            this.AddBytesLabel.Name = "AddBytesLabel";
+            this.AddBytesLabel.Size = new System.Drawing.Size(73, 43);
+            this.AddBytesLabel.TabIndex = 6;
+            this.AddBytesLabel.Text = "Добавить(МБ)";
+            this.AddBytesLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // OutputSizeLabel
+            // 
+            this.OutputSizeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OutputSizeLabel.ForeColor = System.Drawing.Color.Blue;
+            this.OutputSizeLabel.Location = new System.Drawing.Point(12, 230);
+            this.OutputSizeLabel.Name = "OutputSizeLabel";
+            this.OutputSizeLabel.Size = new System.Drawing.Size(168, 43);
+            this.OutputSizeLabel.TabIndex = 7;
+            this.OutputSizeLabel.Text = "Выходной размер: -";
+            this.OutputSizeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(192, 278);
+            this.Controls.Add(this.OutputSizeLabel);
+            this.Controls.Add(this.AddBytesLabel);
+            this.Controls.Add(this.CountMb);
+            this.Controls.Add(this.SaveFile);
+            this.Controls.Add(this.FileSizeLabel);
+            this.Controls.Add(this.FileNameLabel);
+            this.Controls.Add(this.SelectFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MoarBytes";
+            ((System.ComponentModel.ISupportInitialize)(this.CountMb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SelectFile;
+        private System.Windows.Forms.Label FileNameLabel;
+        private System.Windows.Forms.Label FileSizeLabel;
+        private System.Windows.Forms.Button SaveFile;
+        private System.Windows.Forms.NumericUpDown CountMb;
+        private System.Windows.Forms.Label AddBytesLabel;
+        private System.Windows.Forms.Label OutputSizeLabel;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace MoarBytes
+{
+    public partial class MainForm : Form
+    {
+        private string fPath;
+        private BigInteger fSize;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFile_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog fileDialog = new OpenFileDialog())
+            {
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    fPath = fileDialog.FileName;
+                    fSize = GetFileSize(new Uri(fPath));
+
+
+                    FileNameLabel.Text = $"Файл: {fileDialog.SafeFileName}";
+                    FileNameLabel.ForeColor = Color.Green;
+
+                    FileSizeLabel.Text = $"Размер файла: {GetFriendlySize(fSize)}";
+                    OutputSizeLabel.Text = $"Выходной размер: {GetFriendlySize(fSize + GetMoarSize())}";
+                }
+            }
+        }
+
+        private string GetFriendlySize(BigInteger bytes)
+        {
+            string[] suffixes = { "Б", "КБ", "МБ", "ГБ" };
+            int order = 0;
+
+            while (bytes >= 1024 && order < suffixes.Length - 1)
+            {
+                bytes /= 1024;
+                order++;
+            }
+            
+
+            return $"{bytes:0.##} {suffixes[order]}";
+        }
+
+        private BigInteger GetFileSize(Uri uriPath)
+        {
+            var webRequest = WebRequest.Create(uriPath);
+            webRequest.Method = "HEAD";
+            using (var webResponse = webRequest.GetResponse())
+                return BigInteger.Parse(webResponse.Headers.Get("Content-Length"));
+        }
+
+        private BigInteger GetMoarSize() => (BigInteger)CountMb.Value * 1024 * 1024;
+
+        private void RunProcess(string name, string arguments)
+        {
+            Process process = new Process();
+            ProcessStartInfo processInfo = new ProcessStartInfo
+            {
+
+                FileName = name,
+                Arguments = arguments,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            process.StartInfo = processInfo;
+            process.Start();
+            process.WaitForExit();
+        }
+
+
+        private string GetFileNameWithMoared()
+        {
+            string directory = Path.GetDirectoryName(fPath);
+            string fileName = Path.GetFileNameWithoutExtension(fPath);
+            string fileExtension = Path.GetExtension(fPath);
+            return Path.Combine(directory, $"{fileName}_moared{fileExtension}");
+        }
+
+        private void CountMb_ValueChanged(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(fPath))
+                OutputSizeLabel.Text = $"Выходной размер: {GetFriendlySize(fSize + GetMoarSize())}";
+        }
+
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (string.IsNullOrEmpty(fPath))
+                {
+                    MessageBox.Show("Файл не выбран");
+                    return;
+                }
+
+                if(!File.Exists(fPath))
+                {
+                    MessageBox.Show("Файл не существует");
+                    
+                    FileNameLabel.Text = $"Файл: Не выбран";
+                    FileNameLabel.ForeColor = Color.Red;
+
+                    FileSizeLabel.Text = $"Размер файла: -";
+                    OutputSizeLabel.Text = $"Выходной размер: -";
+
+                    return;
+                }
+
+                string fileMoared = GetFileNameWithMoared();
+                if (File.Exists(fileMoared))
+                    File.Delete(fileMoared);
+                File.Copy(fPath, fileMoared);
+                RunProcess("fsutil", $"file seteof \"{fileMoared}\" {GetMoarSize() + fSize}");
+
+                if (GetFileSize(new Uri(fileMoared)) != GetMoarSize() + fSize)
+                {
+                    if (File.Exists(fileMoared))
+                        File.Delete(fileMoared);
+                    MessageBox.Show("Увеличение размера файле не произошло");
+                }
+                else
+                    RunProcess("explorer", $"/e, /select, \"{fileMoared}\"");
+            }
+            catch(IOException)
+            {
+                MessageBox.Show("Файл используется другим процессом");
+            }
+        }
+    }
+}
